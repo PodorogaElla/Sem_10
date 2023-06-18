2b9f98bfeedbb636c685739c4950f936c9ea09f7
PodorogaElla <Elka_forest@mail.ru>
2023-06-18 13:58:09 +0500

    added Task3

--- Sem9_HW/Task3.py
+++ Sem9_HW/Task3.py
@@ -1,2 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
+def mse_ab(a, b, x, y):
+    return np.sum(((a + b * x) - y) ** 2) / len(x)
+def mse_pa(a, b, x, y):
+    return 2 * np.sum((a + b * x) - y) / len(x)
+def mse_pb(a, b, x, y):
+    return 2 * np.sum(((a + b * x) - y) * x) / len(x)
+alpha = 3e-5
+b = 0.1
+a = 0.1
+mseab_min = mse_ab(a, b, zp, ks)
+i_min = 1
+b_min = b
+a_min = a
+iteration = 1000000
+for i in range(iteration):
+    a -= alpha * mse_pa(a, b, zp, ks)
+    b -= alpha * mse_pb(a, b, zp, ks)
+    if i % 50000 == 0:
+        print(f'>>> Итерация #{i}, a={a}, b={b}, mse={mse_ab(a, b, zp, ks)}')
+    if mse_ab(a, b, zp, ks) > mseab_min:
+        print(f'>>> Итерация #{i_min}, a={a_min}, b={b_min}, mse={mseab_min},\nДостигнут минимум.')
+        break
+    else:
+        mseab_min = mse_ab(a, b, zp, ks)
+        i_min = i
+        b_min = b
+        a_min = a
+print(f'>>> a = {a_min}\n>>> b = {b_min}')
+
